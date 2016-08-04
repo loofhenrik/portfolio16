7fd967798a62395fcacb793cd81986617b648778
Henrik Lööf <loof.henrik@telia.com>
2016-08-04 18:02:35 +0200

    lolz2

--- scripts/js.js
+++ scripts/js.js
@@ -8,29 +8,30 @@ if(windowWidth >= 416) {
 }
 
 if(windowWidth <= 415) {
-    $('.toggle_text').click(function () {
-        $(this).parent().children('.img_color').slideToggle(500);
-        $(this).children('.toggle_show').toggle(500);
-        $(this).children('.toggle_hide').toggle(500);
-
-    });
-
-
-    window.onscroll = function () {
-        var y = document.body.scrollTop;
-
-        $('document').ready(function () {
-            var contentTop = document.getElementsByClassName("skills");
-            contentTop = $(contentTop).offset().top;
-
-            if(y > contentTop-100) {
-                $('header').addClass('header_shadow');
-            }
-            else {
-                $('header').removeClass('header_shadow');
-
-            }
-        });
-    };
 
 }
+
+$('.toggle_text').click(function () {
+    $(this).parent().children('.img_color').slideToggle(500);
+    $(this).children('.toggle_show').toggle(500);
+    $(this).children('.toggle_hide').toggle(500);
+
+});
+
+
+window.onscroll = function () {
+    var y = document.body.scrollTop;
+
+    $('document').ready(function () {
+        var contentTop = document.getElementsByClassName("skills");
+        contentTop = $(contentTop).offset().top;
+
+        if(y > contentTop-100) {
+            $('header').addClass('header_shadow');
+        }
+        else {
+            $('header').removeClass('header_shadow');
+
+        }
+    });
+};
