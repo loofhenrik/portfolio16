24e952f0844acc05a6338508d0d33dcbbd7e7d1a
Henrik Lööf <loof.henrik@telia.com>
2016-08-05 09:22:09 +0200

    js bugs

--- scripts/js.js
+++ scripts/js.js
@@ -8,30 +8,30 @@ if(windowWidth >= 416) {
 }
 
 if(windowWidth <= 415) {
+    $('.toggle_text').click(function () {
+        console.log("Toggleclicked");
+        $(this).parent().children('.img_color').slideToggle(500);
+        $(this).children('.toggle_show').toggle(500);
+        $(this).children('.toggle_hide').toggle(500);
 
-}
-
-$('.toggle_text').click(function () {
-    $(this).parent().children('.img_color').slideToggle(500);
-    $(this).children('.toggle_show').toggle(500);
-    $(this).children('.toggle_hide').toggle(500);
-
-});
-
-
-window.onscroll = function () {
-    var y = document.body.scrollTop;
-
-    $('document').ready(function () {
-        var contentTop = document.getElementsByClassName("skills");
-        contentTop = $(contentTop).offset().top;
-
-        if(y > contentTop-100) {
-            $('header').addClass('header_shadow');
-        }
-        else {
-            $('header').removeClass('header_shadow');
-
-        }
     });
-};
+
+
+    window.onscroll = function () {
+        var y = document.body.scrollTop;
+
+        $('document').ready(function () {
+            var contentTop = document.getElementsByClassName("skills");
+            contentTop = $(contentTop).offset().top;
+
+            if(y > contentTop-100) {
+                console.log("scrolly");
+                $('header').addClass('header_shadow');
+            }
+            else {
+                $('header').removeClass('header_shadow');
+
+            }
+        });
+    };
+}
